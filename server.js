01a2a75f99809003792102c9df64801c47c116a9
nikhilkakumanu143-netizen <nikhilkakumanu143@gmail.com>
2026-05-09 20:37:45 +0530

    fixed duplicate server code

--- server.js
+++ server.js
@@ -1,55 +1,32 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '1.1.1.1']);
 
-const taskRoutes = require("./routes/taskroutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`);
+    });
+})
+.catch((err) => {
+    console.log(err);
 });
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '1.1.1.1']);
-
-const taskRoutes = require("./routes/taskroutes");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/tasks", taskRoutes);
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
